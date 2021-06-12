e14ad03b99b7e6c9ea6332c4f8cf5b59921b6059
EneidaLame <Eneida_lame@hotmail.com>
2021-06-12 12:57:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          
+
+            while (true)
+            {
+                Random random = new Random();
+                int guessNumber = random.Next(1, 10);
+
+                int guess = 0;
+                Console.WriteLine("Please guess a number between 1 and 10.");
+
+                while (guess != guessNumber)
+                {
+                    string input = Console.ReadLine();
+                    guess = Int32.Parse(input);
+
+                    if (guess != guessNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("That is not correct. Please try again!");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("You Win!");
+                        Console.ResetColor();
+                    }
+                }
+
+                Console.WriteLine("Do you want to play again? [Y or N]");
+
+                string answer = Console.ReadLine().ToUpper();
+                if (answer == "Y")
+                {
+                    continue;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+        }
+    }
+}
