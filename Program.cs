0e28390d2571d398f9076ef0512f654fc6e554c6
EneidaLame <Eneida_lame@hotmail.com>
2021-06-13 11:56:54 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -10,49 +10,54 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
-            
-          
 
-            while (true)
-            {
-                Random random = new Random();
-                int guessNumber = random.Next(1, 10);
+            StartGame();
 
-                int guess = 0;
-                Console.WriteLine("Please guess a number between 1 and 10.");
 
-                while (guess != guessNumber)
+        }
+        public static void StartGame()
+        {
+                while (true)
                 {
-                    string input = Console.ReadLine();
-                    guess = Int32.Parse(input);
+                    Random random = new Random();
+                    int guessNumber = random.Next(1, 10);
 
-                    if (guess != guessNumber)
+                    int guess = 0;
+                    Console.WriteLine("Please guess a number between 1 and 10.");
+
+                    while (guess != guessNumber)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("That is not correct. Please try again!");
-                        Console.ResetColor();
+                        string input = Console.ReadLine();
+                        guess = Int32.Parse(input);
+
+                        if (guess != guessNumber)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("That is not correct. Please try again!");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("You Win!");
+                            Console.ResetColor();
+                        }
+                    }
+
+                    Console.WriteLine("Do you want to play again? [Y or N]");
+
+                    string answer = Console.ReadLine().ToUpper();
+                    if (answer == "Y")
+                    {
+                        continue;
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("You Win!");
-                        Console.ResetColor();
+                        return;
                     }
                 }
 
-                Console.WriteLine("Do you want to play again? [Y or N]");
-
-                string answer = Console.ReadLine().ToUpper();
-                if (answer == "Y")
-                {
-                    continue;
-                }
-                else
-                {
-                    return;
-                }
-            }
-
+            
         }
     }
 }
